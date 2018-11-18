8344dce703b9d59704050d59a669cadde427ffe4
Claudia Fox <claudiafox95@icloud.com>
2018-11-18 20:25:27 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
 esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  esperanto_arr = esperanto.chars
+  esperanto_arr = esperanto_alphabet.chars
   arr.sort_by do |words|
     words.split('').collect do |letter|
       esperanto_arr.index(letter)
